43e5a12623bdd8c6e4079c50a6d3416b8145b086
DevilTheCrypto <91038639+DevilTheCrypto@users.noreply.github.com>
2021-12-06 23:15:36 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/DevilPortalV2/precache-manifest.6b2c377b799f9f1451bc284c858ea458.js"
+  "/DevilPortalV2/precache-manifest.6daef7829778e85980299325e113f8a3.js"
 );
 
 workbox.clientsClaim();
